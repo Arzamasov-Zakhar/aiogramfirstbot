50f9c804a2398c25feb1b13c64c5d1ae871468f8
Arzamasov-Zakhar <arzamasov_z@mail.ru>
2022-11-23 23:12:16 +0300

    NEW

--- aiogram_bot_reminders.py
+++ aiogram_bot_reminders.py
@@ -2,8 +2,11 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from decouple import config
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 import aioschedule
+import sqlite3
+from datetime import datetime, timedelta
+
 
 
 TOKEN = config("TOKEN")
@@ -11,26 +14,101 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-time1 = 1
+conn = sqlite3.connect('db_aiogram_tg.db', check_same_thread=False)
+cursor = conn.cursor()
+time1 = None
 
 
-async def reminder():
-    builder = InlineKeyboardBuilder()
-    builder.row(types.InlineKeyboardButton(
-        text=config("reminder_text"),
-        url=config("reminder_text")))
+async def check_for_changes():
+    async def get_reminders():
+        but1 = cursor.execute(f"SELECT name FROM buttons WHERE buttom = 'reminder_button'")
+        but1 = cursor.fetchone()
+        but11 = cursor.execute(f"SELECT url FROM buttons WHERE buttom = 'reminder_button'")
+        but11 = cursor.fetchone()
+        builder = InlineKeyboardBuilder()
+        if but1[0] != """off""":
+            builder.row(types.InlineKeyboardButton(
+                text=but1[0],
+                url=but11[0]))
 
-    await bot.send_message(chat_id=config("chat_id"),
-                           text=f"""Dear Customers, beware of fraudsters! 
-Our team will never message you first or request a fund transfer anywhere except the {config("reminder_message1")} website. There is no “manual mode” for transactions, everything goes through the website!""",
-                           reply_markup=builder.as_markup())
+        notification1 = cursor.execute(f"SELECT time, text FROM table_info WHERE name = 'notification'")
+        notification1 = cursor.fetchone()
+        await bot.send_message(chat_id=config("chat_id"),
+                               text=f"""{notification1[1]}""",
+                               reply_markup=builder.as_markup())
+
+    time_new = cursor.execute(f"SELECT new FROM reminders WHERE name = 'reminder'")
+    time_new = cursor.fetchone()
+    time_old = cursor.execute(f"SELECT old FROM reminders WHERE name = 'reminder'")
+    time_old = cursor.fetchone()
+    if time_new[0] == 0:
+        cursor.execute(fr"""UPDATE reminders SET old = {time_new[0]} WHERE name = "reminder" """)
+        conn.commit()
+        pass
+    else:
+        if time_new[0] != time_old[0]:
+            cursor.execute(fr"""UPDATE reminders SET old = {time_new[0]} WHERE name = "reminder" """)
+            conn.commit()
+            datetime_write = datetime.now() + timedelta(hours=time_new[0])
+            str_dt_write = datetime_write.strftime('%d.%m.%Y %H:%M:%S')
+            cursor.execute(fr"""UPDATE reminders SET date = "{str_dt_write}" WHERE name = "reminder" """)
+            conn.commit()
+        elif time_new[0] == time_old[0]:
+            time_reminder = cursor.execute(f"""SELECT date FROM reminders WHERE name = "reminder" """)
+            time_reminder = cursor.fetchone()
+            time_remind = datetime.strptime(time_reminder[0], '%d.%m.%Y %H:%M:%S')
+            if datetime.now().strftime('%d.%m.%Y %H:%M') == time_remind.strftime('%d.%m.%Y %H:%M'):
+                new_date = time_remind + timedelta(hours=time_new[0])
+                new_date = new_date.strftime('%d.%m.%Y %H:%M:%S')
+                cursor.execute(fr"""UPDATE reminders SET date = "{new_date}" WHERE name = "reminder" """)
+                conn.commit()
+
+                await get_reminders()
 
 
 async def main():
-    aioschedule.every(time1).hours.do(reminder)
+    aioschedule.every(1).minutes.do(check_for_changes)
 
     while True:
-        await aioschedule.run_pending()
+        try:
+            await aioschedule.run_pending()
+        except:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
